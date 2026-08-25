00493d6fe103f86b84dec4dc9f99d7c2e8d85fa2
megamann81 <luke.ib.bronson@gmail.com>
2026-08-24 19:50:47 -0700

    Update EasyNetheriteIngots.java

--- src/main/java/com/luke/customrecipe/EasyNetheriteIngots.java
+++ src/main/java/com/luke/customrecipe/EasyNetheriteIngots.java
@@ -1,1 +1,16 @@
+package com.luke.customrecipe; 
 
+import org.bukkit.plugin.java.JavaPlugin; 
+
+public class EasyNetheriteIngots extends JavaPlugin {
+@Override
+public void onEnable() {
+getLogger().info("Easy Netherite Ingots has been enabled!");
+CustomNetheriteRecipe.registerRecipe(this);
+} 
+
+@Override
+public void onDisable() {
+getLogger().info("Easy Netherite Ingots has been disabled!");
+}
+}
